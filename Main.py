5534153d19acc7e3c8d276e54029851fc03ab248
Shivam Krishna <50731434+shivamkrishna1000@users.noreply.github.com>
2024-05-18 01:02:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,3 +7,19 @@ import seaborn as sns
 #Loading the Dataset in Pandas DataFrame
 sonar=pd.read_csv('Sonar data.csv',header=None)
 sonar.head()
+sonar.shape
+sonar.describe()
+sonar.value_counts(60)
+sonar.groupby(60).mean()
+x=sonar.drop(columns=60,axis=1)
+y=sonar[60]
+#separating the training and test data
+trainx, testx, trainy, testy=train_test_split(x,y,test_size=0.1,stratify=y, random_state=1)
+trainx.shape
+testx.shape
+model=LogisticRegression()
+#training the model
+model.fit(trainx,trainy)
+#Finding Accuracy on training data
+predictiontrain=model.predict(trainx)
+trainingaccuracy=accuracy_score(predictiontrain,trainy)
